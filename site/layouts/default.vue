28522bf4b4605c7594377760a096963a8f622c77
Ag2SO4pro <2294881615@qq.com>
2022-10-11 15:38:30 +0800

    optimize login behaviour

--- site/layouts/default.vue
+++ site/layouts/default.vue
@@ -9,5 +9,25 @@
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    this.$store.commit('env/setDomain', window.location.href)
+    if (!this.$cookies.get('userToken'))
+      this.$axios
+        .post('/api/login/signin', { ref: window.location.href })
+        .then((res) => {
+          this.$store.commit('user/setCurrent', res.user)
+          this.$store.commit('user/setUserToken', res.token)
+          const config = this.$store.state.config.config
+          this.$cookies.set('userToken', res.token, {
+            maxAge: 86400 * config.tokenExpireDays,
+            path: '/',
+          })
+          window.location = '/'
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+  },
+}
 </script>
