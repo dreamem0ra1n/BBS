d1af493cc2e84660a4cd50e124e0f83c6720cf5f
Ag2SO4pro <2294881615@qq.com>
2022-10-12 14:26:59 +0800

    update config

--- site/plugins/axios.js
+++ site/plugins/axios.js
@@ -3,9 +3,11 @@ export default function ({ $axios, app }) {
   let prefix = ''
   try {
     if (window) prefix = '/bbs2'
-  } catch (e) {}
+  } catch (e) {
+    console.log(e)
+  }
   $axios.onRequest((config) => {
-    config.url = prefix + config.url
+     config.url = prefix + config.url
     config.headers.common['X-Client'] = 'bbs-go-site'
     config.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
     const userToken = app.$cookies.get('userToken')
