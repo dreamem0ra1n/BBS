f29c1571dc780c43766715442a1943a4d057b83d
Ag2SO4pro <2294881615@qq.com>
2022-10-12 20:55:36 +0800

    set for production

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -95,6 +95,7 @@ export const asyncRoutes = [
 const createRouter = () =>
   new Router({
     mode: 'hash', // require service support
+    base:'/bbsadmin/',
     scrollBehavior: () => ({ y: 0 }),
     routes: constantRoutes,
   });
--- admin/vue.config.js
+++ admin/vue.config.js
@@ -4,5 +4,5 @@ module.exports = {
   devServer:{
     port:8080,
     allowedHosts:['www.qsc.zju.edu.cn','localhost'],
-  }
+  },
 };
