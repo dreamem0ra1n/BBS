459074ccba6b426c7aa5acbee96a9174422c40dd
Ag2SO4pro <2294881615@qq.com>
2022-10-13 15:36:50 +0800

    update

--- site/layouts/default.vue
+++ site/layouts/default.vue
@@ -11,31 +11,9 @@
 <script>
 export default {
   mounted() {
-    console.log(this.$route)
-    if (!this.$cookies.get('userToken'))
-      this.$axios
-        .post('/api/login/signin', { ref: null })
-        .then(async (res) => {
-          const pattern = res.user.roles[0].split('_')
-          const role = pattern[0]
-          const node = await this.$axios.get(
-            '/api/topic/node?nodeId=' + pattern[1]
-          )
-          if (node) {
-            const session = node.name
-            res.user.position = session + '-' + role
-          } else res.user.position = res.user.roles[0]
-          this.$store.commit('user/setCurrent', res.user)
-          this.$store.commit('user/setUserToken', res.token)
-          const config = this.$store.state.config.config
-          this.$cookies.set('userToken', res.token, {
-            maxAge: 86400 * config.tokenExpireDays,
-            path: '/',
-          })
-        })
-        .catch((e) => {
-          console.log(e)
-        })
+    // if (!this.$cookies.get('userToken'))
+    this.$store.dispatch('user/signin')
+    console.log(this.$store.state.user)
   },
 }
 </script>
