a464754fcd287cca59bd3f77aef00e91199ecde6
Ag2SO4pro <2294881615@qq.com>
2022-10-13 22:07:11 +0800

    fix:logout in admin

--- admin/src/store/modules/user.js
+++ admin/src/store/modules/user.js
@@ -78,7 +78,7 @@ const actions = {
   // user logout
   async logout({ commit, state, dispatch }) {
     await this._vm.axios.get("/api/login/signout");
-    await this._vm.axios.get("https://www.qsc.zju.edu.cn/passport/v4/logout")
+    await fetch("https://www.qsc.zju.edu.cn/passport/v4/logout");
     commit("SET_TOKEN", "");
     commit("SET_ROLES", []);
     removeToken();
