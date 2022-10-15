2e1c0a148e424faef79e83b8b23e41dfe6c03122
Ag2SO4pro <2294881615@qq.com>
2022-10-16 00:21:05 +0800

    fix:site url in admin&bg tag

--- site/components/MyFooter.vue
+++ site/components/MyFooter.vue
@@ -28,7 +28,7 @@ export default {
           title: '我要BG',
           content: '我要BG',
           access_lv: 1,
-          tags: [7],
+          tags: '7',
           imageList: '',
         })
         this.$msg({
