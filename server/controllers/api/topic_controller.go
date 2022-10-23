e937cf1318953ecdf18173feb880441323c251be
Isshiki_Hugh <2511524185@qq.com>
2022-10-23 17:49:16 +0800

    debug

--- server/controllers/api/topic_controller.go
+++ server/controllers/api/topic_controller.go
@@ -14,7 +14,6 @@ import (
 	"github.com/mlogclub/simple/sqls"
 	"github.com/mlogclub/simple/web"
 	"github.com/mlogclub/simple/web/params"
-	"github.com/sirupsen/logrus"
 
 	"bbs-go/cache"
 	"bbs-go/controllers/render"
@@ -271,8 +270,8 @@ func (c *TopicController) PostTopicsnt() *web.JsonResult {
 		return web.JsonError(err2)
 	}
 	topics, cursor, hasMore := services.TopicService.GetTopicsByNodeIdAndTag(tagId, nodeId, cursor)
-	logrus.Info("finish query from db")
-	return web.JsonCursorData(render.BuildSimpleTopics(topics, user), strconv.FormatInt(cursor, 10), hasMore)
+	ret := web.JsonCursorData(render.BuildSimpleTopics(topics, user), strconv.FormatInt(cursor, 10), hasMore)
+	return ret
 }
 
 // 标签帖子列表
