aa659f4966013cb5e02d3863581c948432bab954
cy <740310894@qq.com>
2023-02-01 00:55:19 +0800

    little fix

--- server/controllers/render/topic_render.go
+++ server/controllers/render/topic_render.go
@@ -113,7 +113,9 @@ func _buildTopic(user *model.User, topic *model.Topic, buildContent bool) *model
 
 		// 加入权限控制
 		if (user == nil) || (topic.AccessLv > 0 && !user.HasAnyRole("高管", "oldbbs_readall")) {
+			rsp.Title = "【无权限】" + rsp.Title
 			rsp.Content = " 🚫 抱歉，您无权访问该帖子的内容！"
+			rsp.TopicId = -1
 		}
 
 		rsp.Node = &model.NodeResponse{
