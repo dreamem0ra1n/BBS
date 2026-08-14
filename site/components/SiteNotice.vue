1a0100797450b8cce91d5b5486484459d36501a1
dreamem0ra1n <3397869281@qq.com>
2026-08-14 14:38:24 +0800

    feat:增加首页公告折叠功能

--- site/components/SiteNotice.vue
+++ site/components/SiteNotice.vue
@@ -1,7 +1,16 @@
 <template>
   <div v-if="config.siteNotification" class="widget">
-    <div class="widget-header">公告</div>
-    <div class="widget-content content notice-content">
+    <div class="widget-header">
+      <span>公告</span>
+      <button
+        class="notice-toggle"
+        type="button"
+        @click="collapsed = !collapsed"
+      >
+        {{ collapsed ? '展开' : '收起' }}
+      </button>
+    </div>
+    <div class="widget-content content notice-content" :class="{ collapsed }">
       <p v-html="config.siteNotification"></p>
     </div>
   </div>
@@ -9,6 +18,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      collapsed: true,
+    }
+  },
   computed: {
     config() {
       return this.$store.state.config.config
@@ -20,5 +34,19 @@ export default {
 <style lang="scss" scoped>
 .notice-content {
   font-size: 80%;
+  overflow: hidden;
+
+  &.collapsed {
+    max-height: 120px;
+  }
+}
+
+.notice-toggle {
+  padding: 0;
+  border: 0;
+  background: transparent;
+  color: var(--text-color3);
+  cursor: pointer;
+  font-size: 12px;
 }
 </style>
